1da4e3daa56f2962dddfff0e950fb55e95fcac46
xuanvi26 <xuanvi.trinhnguyen@gmail.com>
2018-06-13 17:01:51 -0400

    Complete loadedDie exercise

---
+++ week1/countdown.js
@@ -0,0 +1,11 @@
+var countdownGenerator = function (num) {
+    /* your code here */
+  };
+  
+  var countdown = countdownGenerator(3);
+  countdown(); // T-minus 3...
+  countdown(); // T-minus 2...
+  countdown(); // T-minus 1...
+  countdown(); // Blast Off!
+  countdown(); // Rockets already gone, bub!
+  countdown(); // Rockets already gone, bub!
---
+++ week1/loadedDie.js
@@ -0,0 +1,17 @@
+function makeLoadedDie() {
+    var list = [5, 4, 6, 1, 6, 4, 2, 3, 3, 5];
+    var counter = -1;
+
+    return function() {
+      /* your code here */
+      counter += 1;
+      var elem = list[counter]
+      return elem;
+    }
+  }
+  
+  var rollLoadedDie = makeLoadedDie();
+  
+  console.log(rollLoadedDie());  // 5
+  console.log(rollLoadedDie());  // 4
+  console.log(rollLoadedDie());  // 6
