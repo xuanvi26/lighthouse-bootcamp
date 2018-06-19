14e6fde617bf850bb6a10a9455b3e249a01ae21c
xuanvi26 <xuanvi.trinhnguyen@gmail.com>
2018-06-19 12:31:21 -0400

    Test JSON methods

---
+++ week2/weekend/JSONtest.js
@@ -0,0 +1,11 @@
+var str = '{"a":1, "b":2, "foo":"bar"}'; // string version of a JS Object
+var obj = JSON.parse(str);
+
+console.log(typeof(obj));
+console.log(obj);     // an Object that has been deserialized
+
+delete obj.foo;       // modify the object
+var string = JSON.stringify(obj);  // serialize it back to a string
+
+console.log(typeof(string));
+console.log(string);
