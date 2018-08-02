004be8250b31fbbc5f5f595d532d933aad6e8a2f
xuanvi26 <xuanvi.trinhnguyen@gmail.com>
2018-08-02 17:28:31 -0400

    Delete test search function

--- week5/arraySearch.js
+++ week5/arraySearch.js
@@ -26,31 +26,5 @@ const searchBinary = (array, item) => {
     return null;
 }
 
-function search(array, item) {
-    let min = 0;
-    let max = array.length - 1;
-  
-    while (true) {
-      const middleIndex = Math.floor((min + max)/2.0);
-      const currentItem = array[middleIndex];
-  
-      if (currentItem === item) {
-        // Found it
-        return middleIndex;
-      } else if (currentItem < item) {
-        // Look at the right side
-        min = middleIndex + 1;
-      } else {
-        // Look at the left side
-        max = middleIndex - 1;
-      }
-  
-      if (min > max) {
-        return null;
-      }
-    }
-  }
-
-// console.log(searchLinear([1, 2, 3], 3));
 console.log(searchBinary([1, 2, 3, 4, 5, 6, 7], 3));
 console.log(search([1, 2, 3, 4, 5, 6, 7], 3));
