bad9ccd9088239f6b65d074a86015cf194ed963a
xuanvi26 <xuanvi.trinhnguyen@gmail.com>
2018-08-21 18:23:44 -0400

    Complete migrations

--- week4/navy_schema.sql
+++ week4/navy_schema.sql
@@ -9,3 +9,32 @@ CREATE TABLE ship (
     date_built DATE,
     fleet_id int references fleet(id)
 );
+
+CREATE TABLE sailor (
+    id int PRIMARY KEY,
+    name VARCHAR(50),
+    date_birth DATE
+);
+
+CREATE TABLE ship_sailor (
+    sailor_id int references sailor(id),
+    ship_id int references ship(id)
+);
+
+CREATE TABLE assignment (
+    id int PRIMARY KEY,
+    date_start DATE,
+    date_end DATE,
+    name VARCHAR(50),
+    rank int
+);
+
+CREATE TABLE rank (
+    id int PRIMARY KEY,
+    name VARCHAR (50)
+);
+
+CREATE TABLE assignment_rank (
+    assignment_id int references assignment(id),
+    rank_id int references rank(id)
+);
--- week4/navy_schema_down.sql
+++ week4/navy_schema_down.sql
@@ -1,2 +1,7 @@
-drop table fleet;
+drop table ship_sailor;
 drop table ship;
+drop table sailor;
+drop table fleet;
+drop table assignment_rank;
+drop table assignment;
+drop table rank;
