67c5eae092f68ddba3053387f20c58820c37cc24
xuanvi26 <xuanvi.trinhnguyen@gmail.com>
2019-04-23 16:51:42 -0400

    Test JSON parse and stringify methods

---
+++ review/shortExamples.js
@@ -0,0 +1,9 @@
+var str = '{"a":1, "b":2, "foo":"bar"}'; // string version of a JS Object
+var obj = JSON.parse(str);
+
+console.log(obj);     // an Object that has been deserialized
+
+delete obj.foo;       // modify the object
+var str2= JSON.stringify(obj);  // serialize it back to a string
+
+console.log(str2, typeof str2)
