8d25dd0f4f53c2e0e62a3359750194bcbafc1281
xuanvi26 <xuanvi.trinhnguyen@gmail.com>
2019-04-26 15:14:38 -0400

    Fix wrong request method

--- review/streams/httpRequestExercise.js
+++ review/streams/httpRequestExercise.js
@@ -2,30 +2,29 @@ var https = require("https");
 
 function getAndPrintHTMLChunks () {
 
-    var requestOptions = {
-      host: 'sytantris.github.io',
-      path: '/http-examples/step1.html'
-    };
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step2.html'
+  };
 
-    // var dataStr = '';
+    var dataStr = '';
   
-    https.request(requestOptions, (response) => {
+    https.get(requestOptions, (response) => {
 
         response.setEncoding('utf8');
 
         response.on('data', (data) => {
-            // dataStr += data;
-            console.log(data);
-            console.log('\n');
+            dataStr += data;
+            // console.log(data);
+            // console.log('\n');
         })
 
         response.on('end', () => {
+          console.log(dataStr);
             console.log('Request completed')
         })
 
     });
-
-    // console.log(dataStr);
   
   }
 
