8fe4452de7cc0c4615cf4e5903309907467f89b9
xuanvi26 <xuanvi.trinhnguyen@gmail.com>
2019-06-05 15:37:10 -0400

    Create very basic array test

---
+++ review/unit-testing/test/arrayTest.js
@@ -0,0 +1,9 @@
+var chai = require('chai');
+var assert = chai.assert;
+
+describe('Array', function() {
+    it('should start empty', function() {
+        var arr = [];
+        assert.equal(arr.length, 0, 'Array length was not 0');
+    });
+});
