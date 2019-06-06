d87653d3c8bbd7af639121bde9908de278d45c5a
xuanvi26 <xuanvi.trinhnguyen@gmail.com>
2019-06-06 17:51:04 -0400

    Test var and let scoping

--- review/shortExamples.js
+++ review/shortExamples.js
@@ -1,10 +1,29 @@
 var str = '{"a":1, "b":2, "foo":"bar"}'; // string version of a JS Object
 var obj = JSON.parse(str);
 
-console.log(obj);     // an Object that has been deserialized
+// console.log(obj);     // an Object that has been deserialized
 
 delete obj.foo;       // modify the object
 var str2= JSON.stringify(obj);  // serialize it back to a string
 
-console.log(str2, typeof str2)
+// console.log(str2, typeof str2)
 
+function foo() {
+    console.log( x );
+    console.log(hello)
+    // console.log( y );
+  
+    var x = 1;
+    if (x === 1) {
+      let y = 2;
+    }
+    // console.log( y );
+
+    function hello() {
+        console.log(x)
+    }
+
+  }
+  
+  foo();
+//   console.log( x );
